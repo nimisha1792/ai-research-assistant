cc1a70cc62ff7611e663f167c081db0fd02e70fa
Nimisha Anand <nimisha.anand1702@gmail.com>
2026-06-03 14:44:04 +0530

    Added API validation and routing concepts

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,40 @@
 from fastapi import FastAPI
 import asyncio
+from pydantic import BaseModel
+from fastapi import HTTPException
 
 app = FastAPI()
+notes = []
+
+class Note(BaseModel):
+    title: str
+    content: str
 
 @app.get("/")
 async def home():
     await asyncio.sleep(2)
     return {"message": "Async FastAPI Working"}
+
+@app.get("/notes/{note_id}")
+async def get_note(note_id: int):
+    if note_id > len(notes):
+        raise HTTPException(
+            status_code=404,
+            detail="Note not found"
+        )
+    return notes[note_id - 1]
+
+@app.post("/notes")
+async def create_note(note: Note):
+    notes.append(note.dict())
+    return {"message": "Note created successfully", "note": note}
+
+@app.get("/search")
+async def search_notes(query: str):
+    return {
+        "search": query
+    }
+
+@app.get("/error")
+async def error_demo():
+    raise Exception("Something went wrong")
